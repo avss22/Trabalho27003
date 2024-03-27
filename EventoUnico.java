5c70e8403645f66d702d07d7aca43dd3a0c6c1d6
avss22 <163359662+avss22@users.noreply.github.com>
2024-03-27 20:55:09 -0300

    EventoUnico

---
+++ EventoUnico.java
@@ -0,0 +1,9 @@
+
+public class EventoUnico {
+	class EventoUnico extends Evento {
+	    public EventoUnico(String titulo, String descricao, String dataInicio, String dataFim, String localizacao) {
+	        super(titulo, descricao, dataInicio, dataFim, localizacao);
+	    }
+	}
+
+	
